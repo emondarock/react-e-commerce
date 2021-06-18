2750afc0e2034e0cb62259d3fa293bb606bef1eb
emondarock@gmail.com <3m0nd4r0ck>
2021-06-18 17:33:51 +0600

    Add router to our menu-item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import HomePageComponent from "./pages/homepage/homepage";
 import './App.css'
+import {Route, Switch} from "react-router-dom";
+
+const HatPage = () => {
+    return <div>
+        <h1>Hats Page</h1>
+    </div>
+}
 
 function App() {
   return (
     <div>
-      <HomePageComponent/>
+      <Switch>
+          <Route exact path={'/'} component={HomePageComponent}/>
+          <Route path={'/hats'} component={HatPage}/>
+      </Switch>
     </div>
   );
 }
